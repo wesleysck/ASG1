01c79a266ddc9b6a1ad3c3feb22e50306cd438e9
Wesley Siu <s10243612@connect.np.edu.sg>
2022-11-30 20:25:29 +0800

    moved js to the correct file

--- main.js
+++ main.js
@@ -9,23 +9,3 @@ menu.addEventListener('click', function(){
     menuLinks.classList.toggle('active');
 });
 
-// MENU BANNER EFFECT
-
-var image = document.querySelector(".products-banner");
-
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
-
-function moveBackground(event){
-    var mouseXposition = event.clientX;
-    var mouseYposition = event.clientY;
-
-    var calculatedX = mouseXposition / (windowWidth / 7);
-    var calculatedY = mouseYposition / (windowHeight / 7);
-
-    image.style.transform="translate(-"+calculatedX.toString()+"%,-"+calculatedY.toString()+"%)";
-
-
-}
-
-document.addEventListener("mousemove", moveBackground)
