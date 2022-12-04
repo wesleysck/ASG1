b4250f77a9f37da8419119c3be71ba8317fff1eb
Wesley Siu <s10243612@connect.np.edu.sg>
2022-12-04 14:35:48 +0800

    removed unneccesary line

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
 
 menu.addEventListener('click', function(){
-    console.log('clicked');
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
